8d342ed2c3e04944106c2349a0189789d23383f2
pya-h <p4ya.py@gmail.com>
2024-03-23 20:31:58 +0330

    Starting out;

---
+++ HoursWorked/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Collections.Generic;
+
+namespace HoursWorked
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Dictionary<string, Staff> staffs;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.staffs = new Dictionary<string, Staff>();
+            
+        }
+
+
+        public string SelectStaffFile()
+        {
+            // Configure open file dialog box
+            var dialog = new Microsoft.Win32.OpenFileDialog
+            {
+                FileName = "Document", // Default file name
+                DefaultExt = ".txt", // Default file extension
+                Filter = "Text documents (.txt)|*.txt" // Filter files by extension
+            };
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+                return dialog.FileName;
+            throw new Exception("برنامه فادر به باز کردن فایل انتخابی نبود!");
+        }
+
+        static void ReadStaffData(string filePath)
+        {
+            // Open the file
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                // Read each line
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Split the line by spaces
+                    string[] elements = line.Split(' ');
+                    Staff next = new Staff(elements[0]);
+                    // Print each element
+                    foreach (string element in elements)
+                    {
+                        Console.WriteLine(element);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HoursWorked/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HoursWorked
+{
+    class Staff
+    {
+
+        public string Name { get; set; }
+
+        public Staff(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
